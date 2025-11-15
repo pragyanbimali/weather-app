dc2030f96bbe7e6a9854fbdcd7eee6d283a774a1
Pragyan Bimali <65102129+pragyanbimali@users.noreply.github.com>
2025-11-15 21:07:59 +1100

    Delete src directory

--- src/getLocationFromUser.js
+++
@@ -1,5 +0,0 @@
-export const getLocationFromUser = function() {
-    const locationInput = document.querySelector('#location')
-    console.log(locationInput.value);
-    return locationInput.value;
-}
