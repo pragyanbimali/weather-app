537d9be629aac0daee4ca690e590f65734b73abe
Pragyan Bimali <pragyanb02@gmail.com>
2025-11-15 21:20:40 +1100

    Deployment commit

---
+++ 5570a48585c6e4e07f54.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+import { fetch_weather } from './fetch.js';
+import { decryptWeatherJSON, displayWeather } from './decryptWeatherJSON.js'
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const result = await fetch_weather();
+    console.log(result);
+
+    const weatherObj = decryptWeatherJSON(result);
+
+    displayWeather(weatherObj);
+});
+
